aa90bbde335ecbcdcef3b934f9222d7b7d53811c
Kamil Ronda <kamil.ronda@gmail.com>
2020-10-28 22:07:32 +0000

    changed from local to online JSON data. Added movies form

--- src/components/create.js
+++ src/components/create.js
@@ -5,10 +5,69 @@ import '../App.css';
 
 //exports Create which is used in app.js file
 export class Create extends React.Component{
+
+    constructor(){
+      super();
+
+      //these properties bind methods to their Events
+      this.onSubmit = this.onSubmit.bind(this);
+      this.onChangeTitle = this.onChangeTitle.bind(this);
+      this.onChangeYear = this.onChangeYear.bind(this);
+      this.onChangePoster = this.onChangePoster.bind(this);
+
+      this.state ={
+        Title: '',
+        Year: '',
+        Poster: ''
+      }
+    }
+
+  //Methods
+    onChangeTitle(e){
+      this.setState({
+        Title: e.target.value
+      });
+    }
+
+    onChangeYear(e){
+      this.setState({
+        Year: e.target.value
+      });
+    }
+
+    onChangePoster(e){
+      this.setState({
+        Poster: e.target.value
+      });
+    }
+
+    onSubmit(e){
+      e.preventDefault();
+      alert("Movie: " + this.state.Title + " " + this.state.Year + " " + this.state.Poster);
+    }
+
+
+    //form which allows user to enter the movie details 
     render(){
       return (
         <div className="App">
-            <h1>This is the create component.</h1>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <label>Add Movie Title: </label>
+                <input type='text' className='form-control' value={this.state.Title} onChange={this.onChangeTitle}></input>
+              </div>
+              <div className="form-group">
+                <label>Add Movie Year: </label>
+                <input type="text" className='form-control' value={this.state.Year} onChange={this.onChangeYear}></input>
+              </div>
+              <div className="form-group">
+                <label>Movies Poster: </label>
+                <textarea type='text' className='form-control' value={this.state.Poster} onChange={this.onChangePoster}></textarea>
+              </div>
+              <div className="form-group">
+                <input type='submit' value='Add Movie' className='btn btn-primary'></input>
+              </div>
+            </form>
         </div>
       );
     }
